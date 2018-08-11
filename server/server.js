7de4e28e2317bd5c79284fe745a87f03bd1bc8af
Allan Chang (bohan) <bohanzhang@my.unt.edu>
2018-08-11 15:57:31 -0500

    add GET

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ var { Todo }  = require('./models/todo');
 
 var app = express();
 
-
 app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
   var todo = new Todo({
@@ -20,6 +19,13 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos',(req,res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
