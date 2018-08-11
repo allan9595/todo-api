e36e22cf72f379f32afabdbff5cc74f155462e08
Allan Chang (bohan) <bohanzhang@my.unt.edu>
2018-08-11 14:45:23 -0500

    add unit test for server.js, create post end point

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User', {
+  email: {
+    type:String,
+    trim: true,
+    required:true,
+    minLength:1
+  }
+});
+
+module.exports = {
+  User
+};
---
+++ server/tests/server.test.js
@@ -0,0 +1,48 @@
+const expect = require('expect');
+const request = require('supertest');
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+}); //lifecycle wipe all the existing data
+
+describe('Post /todos', ()=>{
+  it('should create a new todo',(done)=>{
+    var text = 'Test todo text';
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+
+  it('should not create an invalid todo', (done) => {
+    var text = '';
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(400)
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      })
+  });
+});
