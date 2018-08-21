73ebd3475a7dd6de8b8cb75d367fa3e9ac13d68c
Allan Chang (bohan) <bohanzhang@my.unt.edu>
2018-08-21 17:38:15 -0500

    add config

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 const _ = require('lodash');
@@ -6,7 +8,7 @@ var { User } = require('./models/user');
 var { Todo }  = require('./models/todo');
 const { ObjectID } = require('mongodb');
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
   var todo = new Todo({
